d92d325c53cc3ad7387f5e6ee1dd43a7bd21efbb
우정호 <somej@naver.com>
2025-02-20 23:16:58 +0900

    login controller추가가

---
+++ src/main/java/com/home/evuela/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.home.evuela.login;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
